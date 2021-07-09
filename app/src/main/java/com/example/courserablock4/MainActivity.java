d53b521ed3ab4f12f71bcdb53742d974a931cd5b
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-09 08:44:51 +0530

    Created MediaPlayer by attaching piano music

--- app/src/main/java/com/example/courserablock4/MainActivity.java
+++ app/src/main/java/com/example/courserablock4/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pianoMusic = MediaPlayer.create(this, R.raw.piano);
     }
 
     public void playMusic(View view){
